a07196d0057e445eedd7da669a8f99213a5950e9
JiiTee <juha.tiihonen@gmail.com>
2022-01-05 00:44:50 +0200

    part 3 full stack + crlf

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
@@ -14,60 +14,62 @@ const App = () => {
   const [nameFilter, setNameFilter] = useState('')
   const [notification, setNotification] = useState(null)
 
-  const messageDuration = 3000;
-  
+  const messageDuration = 5000;
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)   
+    setNewNumber(event.target.value)
   }
 
   const handleNameFilterChange = (event) => {
-    setNameFilter(event.target.value)   
+    setNameFilter(event.target.value)
   }
 
   const addPerson = (event) => {
     event.preventDefault()
     //console.log('button clicked', event.target.value)
-    
+
     if (persons.some(person => person.name === newName)) {
       if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
         //TODO should refactor to use only one find
-        const oldPerson = persons.find(person =>   person.name === newName)
+        const oldPerson = persons.find(person => person.name === newName)
         const changedPerson = { ...oldPerson, number: newNumber }
         personService
-        .update(oldPerson.id, changedPerson)
-          .then(returnedPerson => {        
-          setPersons(persons.map(person => person.id !== oldPerson.id ? person : returnedPerson))
-          //TODO should refactor to get rid of double input field cealring
-          setNewName('')
-          setNewNumber('')
-          setNotification(
-            { message: `The phone number of '${oldPerson.name}' was changed to '${changedPerson.number}.`,
-              style: 'success'
-            }
-          )
-          setTimeout(() => {
-            setNotification(null)
-          }, messageDuration)
-        })
-        .catch(error => {
-          personService.getAll()
-          .then (retunedPersons => {
-            setPersons(retunedPersons) 
+          .update(oldPerson.id, changedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== oldPerson.id ? person : returnedPerson))
+            //TODO should refactor to get rid of double input field clearing
+            setNewName('')
+            setNewNumber('')
+            setNotification(
+              {
+                message: `The phone number of '${oldPerson.name}' was changed to '${changedPerson.number}.`,
+                style: 'success'
+              }
+            )
+            setTimeout(() => {
+              setNotification(null)
+            }, messageDuration)
           })
-  
-          setNotification(
-            { message: `Information of '${oldPerson.name}' was already removed from server`,
-              style: 'error'
-            }
-          )
-          setTimeout(() => {
-            setNotification(null)
-          }, messageDuration)
-        })
+          .catch(error => {
+            personService.getAll()
+              .then(retunedPersons => {
+                setPersons(retunedPersons)
+              })
+
+            setNotification(
+              {
+                message: `Update failed: ${error.response.data.error}`,
+                style: 'error'
+              }
+            )
+            setTimeout(() => {
+              setNotification(null)
+            }, messageDuration)
+          })
       }
     } else { //normal add
       const personObject = {
@@ -76,53 +78,67 @@ const App = () => {
       }
 
       personService
-      .create(personObject)
-        .then(returnedPerson => {        
-        setPersons(persons.concat(returnedPerson))
-        setNewName('')
-        setNewNumber('')
-        setNotification(
-          { message: `Added '${returnedPerson.name}' with number '${returnedPerson.number}.`,
-            style: 'success'
-          }
-        )
-        setTimeout(() => {
-          setNotification(null)
-        }, messageDuration)
-      })  
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+          setNotification(
+            {
+              message: `Added '${returnedPerson.name}' with number '${returnedPerson.number}.`,
+              style: 'success'
+            }
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, messageDuration)
+        })
+        .catch(error => {
+          setNotification(
+            {
+              message: `Person validation failed: ${error.response.data.error}.`,
+              style: 'error'
+            }
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, messageDuration)
+        })
     }
   }
-  
-  const deletePersonHandler = id  => (event) => {
+
+  const deletePersonHandler = id => (event) => {
     event.preventDefault()
     console.log('delete button clicked', event.target.value, id)
-    const person = persons.find(person =>  person.id === id)
+    const person = persons.find(person => person.id === id)
     if (window.confirm(`Delete ${person.name}?`)) {
       personService
-      .delete(id).then( () => {
-        setPersons(persons.filter(person => person.id !== id))
+        .delete(id).then(() => {
+          setPersons(persons.filter(person => person.id !== id))
 
-        setNotification(
-          { message: `Deleted ${person.name}'.`,
-            style: 'success'
-          }
-        )
-        setTimeout(() => {
-          setNotification(null)
-        }, messageDuration)
-      })
-      .catch(error => {
-        setNotification(
-          { message: `the person with id '${person.name}' was already deleted from server`,
-            style: 'error'
-          }
-        )
-        setTimeout(() => {
-          setNotification(null)
-        }, messageDuration)
-      })
+          setNotification(
+            {
+              message: `Deleted ${person.name}'.`,
+              style: 'success'
+            }
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, messageDuration)
+        })
+        .catch(error => {
+          setNotification(
+            {
+              message: `the person with id '${person.name}' was already deleted from server`,
+              style: 'error'
+            }
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, messageDuration)
+        })
     }
-    
+
   }
 
   const personsToShow = nameFilter === '' ?
@@ -132,33 +148,33 @@ const App = () => {
   useEffect(() => {
     console.log('effect')
     personService.getAll()
-    .then (retunedPersons => {
-      setPersons(retunedPersons) 
-    })
+      .then(retunedPersons => {
+        setPersons(retunedPersons)
+      })
   }, [])
-        
+
   return (
     <div>
       <h2>Phonebook</h2>
 
       <Notification notification={notification} />
 
-      <Filter filterChangeHandler={handleNameFilterChange} value={nameFilter}/>
+      <Filter filterChangeHandler={handleNameFilterChange} value={nameFilter} />
 
       <h3>Add a new</h3>
-      <PersonForm 
-        handleNameChange={handleNameChange} 
+      <PersonForm
+        handleNameChange={handleNameChange}
         nameValue={newName}
         handleNumberChange={handleNumberChange}
         numberValue={newNumber}
         submitHandler={addPerson}
       />
-      
+
       <h3>Numbers</h3>
-      <Persons persons={personsToShow} deleteHandler={deletePersonHandler}/>
-    
-  </div>
-    
+      <Persons persons={personsToShow} deleteHandler={deletePersonHandler} />
+
+    </div>
+
   )
 
 }
